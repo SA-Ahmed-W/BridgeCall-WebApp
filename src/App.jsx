4c313e9bee90c74cde35ef5fcba5df6bc4525202
SAASIMAHMEDW <aasimahmedsiddiqui1@gmail.com>
2025-08-15 22:09:39 +0530

    feat: setup firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+
 
 function App() {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const querySnapshot = await getDocs(collection(db, "test"));
+      if (querySnapshot.empty) {
+        console.log("No documents found in the 'test' collection.");
+        return;
+      }
+      const docsData = [];
+      querySnapshot.forEach(doc => {
+        docsData.push({ id: doc.id, ...doc.data() });
+      });
+  
+      setDocs(docsData);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <h2>Test Collection</h2>
+      <ul>
+        {docs.map(doc => (
+          <li key={doc.id}>
+            <div>
+              <strong>{doc.id}</strong>: {JSON.stringify(doc)}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
